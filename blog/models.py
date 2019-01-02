1e3432fe1833bf6b79fc67b74282d7e8b76cda1b
Creativen <marco@creativen.net>
2019-01-03 03:14:12 +0800

    Extended Tutorials Completed 3 of 3

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
@@ -27,6 +30,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
