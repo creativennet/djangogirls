d5096739f9f2e03d2c70b04aa3dc43ba078d6fc7
Creativen <marco@creativen.net>
2019-01-03 00:15:42 +0800

    Now the blog requires authentication for publish, edit, remove, draft

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+# Redirect to / if login successful
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
